65086f26308cc1e5f15fac0d78ba19ceef990bcc
AT071998 <67892006+AT071998@users.noreply.github.com>
2021-02-20 11:37:00 +0530

    add login post route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('boyd-parser');
+app.use(bodyParser.json());
 //object form
 const userData = [
                   {id:1,name:'RM'},
@@ -11,7 +13,7 @@ app.get('/users/:id',function(req,res){
     res.json({
         users: req.params.id
     })
-    console.log(req.params.id.name);
+   // console.log(req.params.id.name);
     
 })
 app.get('/users',function(req,res){
@@ -19,6 +21,19 @@ app.get('/users',function(req,res){
     const objData = JSON.stringify(userData);
     console.log(objData);
     res.send(objData)
+})
+//login form. . . 
+app.post('/login',function(req,res){
+    const username = req.body.username;
+    const pass = req.body.password;
+
+    const mockuser = "learnerstack";
+    const mockpass = "1234";
+    if(username==mockuser && pass==mockpass){
+        res.json({success:true,message:"Logged in",token:'encrypted token goes here'});
+    }else{
+        res.json({success:false,message:"login failed!"})
+    }
 })
 app.listen(8080,function(){
     console.log("server is running");
