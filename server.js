22b5e399015f10bbba4f07e5bd855da79c500421
AT071998 <67892006+AT071998@users.noreply.github.com>
2021-02-20 11:05:51 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+const userData = [
+                  {name:'RM'},
+                  {name:'Jin' }
+                ]
+
+app.get('/users',function(req,res){
+    res.json({
+        success: true,
+        message: 'succesffuly got the details!',
+        users: userData
+    })
+    //const objData = JSON.parse(userData);
+    //console.log(objData);
+})
+
 app.listen(8080,function(){
     console.log("server is running");
 });
